22f69222e8db12b76928958355bed1a063a0994b
Francesca <fpcantor@gmail.com>
2019-12-27 11:59:40 -0800

    Rename endpoints file

---
+++ basic-endpoints.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import tornado.web
+import tornado.ioloop
+
+class basicRequestHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.write("Hello, world! This is a python command from the backend!")
+
+class queryParamRequestHandler(tornado.web.RequestHandler):
+  def get(self):
+    num = self.get_argument("num")
+    if (num.isdigit()):
+      r = "odd" if int(num) % 2 else "even"
+      self.write(f"{num} is {r}")
+    else:
+      self.write(f"{num} is not a valid integer")
+
+class listRequestHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.render("index.html")
+
+class resourceParamRequestHandler(tornado.web.RequestHandler):
+  def get(self, studentName, courseID):
+    self.write(f"Welcome, {studentName}! This is course number {courseID}")
+
+if __name__ == "__main__":
+  app = tornado.web.Application([
+    (r"/", basicRequestHandler),
+    (r"/animals", listRequestHandler),
+    (r"/isEven", queryParamRequestHandler),
+    (r"/students/([A-Za-z]+)/([0-9]+)", resourceParamRequestHandler)
+  ])
+
+  port = 8882
+  app.listen(port)
+  print(f"Application is ready and listening to {port}")
+  tornado.ioloop.IOLoop.current().start()
